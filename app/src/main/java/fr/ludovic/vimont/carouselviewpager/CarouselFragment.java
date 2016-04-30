0f498a9b42a620deba75f8418482fa317f7a318c
lvimont <ludovic.vimont@gmail.com>
2016-04-30 14:12:10 +0200

    Add sweet animation when description appears & change icons size

--- app/src/main/java/fr/ludovic/vimont/carouselviewpager/CarouselFragment.java
+++ app/src/main/java/fr/ludovic/vimont/carouselviewpager/CarouselFragment.java
@@ -6,7 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -62,25 +63,51 @@ public class CarouselFragment extends Fragment {
         infoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View infoView) {
-                infoView.setVisibility(View.INVISIBLE);
-
                 final RelativeLayout descriptionLayout = (RelativeLayout) root.findViewById(R.id.description_layout);
-                descriptionLayout.setVisibility(View.VISIBLE);
-
-                ImageButton backButton = (ImageButton) root.findViewById(R.id.back_button);
-                backButton.setOnClickListener(new View.OnClickListener() {
+                opacityAnimation(descriptionLayout, 0.0f, 1.0f, 750, true, new Animation.AnimationListener() {
                     @Override
-                    public void onClick(View backView) {
-                        descriptionLayout.setVisibility(View.INVISIBLE);
-                        infoView.setVisibility(View.VISIBLE);
+                    public void onAnimationStart(Animation animation) {
+                        infoView.setVisibility(View.INVISIBLE);
+                        TextView descriptionView = (TextView) root.findViewById(R.id.description_view);
+                        descriptionView.setText(getArguments().getString("description"));
                     }
-                });
 
-                TextView descriptionView = (TextView) root.findViewById(R.id.description_view);
-                descriptionView.setText(getArguments().getString("description"));
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        ImageButton backButton = (ImageButton) root.findViewById(R.id.back_button);
+                        backButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View backView) {
+                                opacityAnimation(descriptionLayout, 1.0f, 0.0f, 750, true, new Animation.AnimationListener() {
+                                    @Override
+                                    public void onAnimationStart(Animation animation) {}
+
+                                    @Override
+                                    public void onAnimationEnd(Animation animation) {
+                                        infoView.setVisibility(View.VISIBLE);
+                                    }
+
+                                    @Override
+                                    public void onAnimationRepeat(Animation animation) {}
+                                });
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {}
+                });
             }
         });
 
         return root;
+    }
+
+    private void opacityAnimation(final View view, float fromAlpha, float toAlpha, int duration, boolean keepResult, Animation.AnimationListener listener){
+        Animation alphaAnimation = new AlphaAnimation(fromAlpha, toAlpha);
+        if(keepResult) alphaAnimation.setFillAfter(true);
+        alphaAnimation.setDuration(duration);
+        alphaAnimation.setAnimationListener(listener);
+        view.startAnimation(alphaAnimation);
     }
 }
